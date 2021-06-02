fec46430fbf3dbd856f51e8c03edeaee4b9cd3a3
Vouyeuz <fahrizal.indi@gmail.com>
2021-06-03 00:35:42 +0700

    Membuat header pada homepage dengan menambahkan file Header.js dan Header.css

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5nv3btHdeuRprSGTr2zs7ta_IujPKCK0",
+  authDomain: "clone-10f53.firebaseapp.com",
+  projectId: "clone-10f53",
+  storageBucket: "clone-10f53.appspot.com",
+  messagingSenderId: "853133672931",
+  appId: "1:853133672931:web:c3c6e9b42e30c2aee4e0d6",
+  measurementId: "G-6EBWWDNYEM",
+};
