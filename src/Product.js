2c602ab141108df88100fc0f6c8033163e655e06
Vouyeuz <fahrizal.indi@gmail.com>
2021-06-03 15:15:08 +0700

    Mengatur style products pada homepage

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>TITLE</p>
+        <p>{title}</p>
 
         <p className="product__price">
-          <small>$</small>
-          <strong>100</strong>
+          <small>Rp</small>
+          <strong>{price}</strong>
         </p>
 
-        <div className="product__rating">*****</div>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
 
-        <img src="" alt="" />
+        <img src={image} alt="" />
 
         <button>Add to Basket</button>
       </div>
