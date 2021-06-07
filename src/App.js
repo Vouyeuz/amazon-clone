7b7dc396d1386fdc220a6b00a17c96428853040f
Vouyeuz <fahrizal.indi@gmail.com>
2021-06-07 17:03:54 +0700

    Menyelesaikan front-end Login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
