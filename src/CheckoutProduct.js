82542d5168da1b464cb55d3c1d3ad860a033c6cf
Vouyeuz <fahrizal.indi@gmail.com>
2021-06-07 07:48:19 +0700

    Menambahkan remove from basket dan menyelesaikan checkout page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -6,6 +6,13 @@ import { useStateValue } from "./StateProvider";
 function CheckoutProduct({ id, image, title, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
 
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="" />
@@ -39,7 +46,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
             ))}
         </div>
 
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
